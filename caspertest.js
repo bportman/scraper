fb3127e38d6d62a178e67aae34af77e7ecbbc962
Benjamin Portman <benjamin@orpheusincorporated.com>
2018-01-14 01:47:44 -0500

    Final commit - this got us complete data!

--- caspertest.js
+++ caspertest.js
@@ -4,21 +4,27 @@ var casper = require('casper').create({
     }
 }); 
 var x = require('casper').selectXPath; 
-var downloadcounter = 0;
+var count;
 casper.userAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36") 
 
 casper.start('https://www.eride.ri.gov/eride2K5/AggregateAttendance/AttendanceReports.aspx', function() {
     for(var j = 0; j < 84; j++) {
-        for(var i = 2;i < 32; i++) {
-            if (i < 10) {
-                var handle = '#GridView1_ctl0'+i+'_imgBtnXls';
-            } else {
-                var handle = '#GridView1_ctl'+i+'_imgBtnXls';
-            }
-
-            if(this.exists(handle)) {
-                this.thenClick(handle);
-                downloadcounter++;
+        count = this.evaluate(function() { 
+            var elements = __utils__.findAll('#GridView1 tr');
+            return elements.length; 
+        });
+        this.echo(count);
+        if(j > 41) { // we had to comment this out first, run til it failed, then uncomment and run again to get complete data
+            for(var i = 2; i <= count; i++) {
+                if (i < 10) {
+                    var handle = '#GridView1_ctl0'+i+'_imgBtnXls';
+                } else {
+                    var handle = '#GridView1_ctl'+i+'_imgBtnXls';
+                }
+                this.echo(handle);
+                if(this.exists(handle)) {
+                    this.thenClick(handle);
+                }
             }
         }
         this.thenClick('a[title="Go to the previous month"]');
@@ -29,7 +35,13 @@ casper.on('resource.received', function(resource) {
     casper.echo(resource.url);
     if (resource.url.indexOf('DailyAbsenceData.csv') > -1) {
         var filenamebits = resource.url.split('/');
-        casper.download(resource.url, filenamebits[filenamebits.length-1]);
+        var filename = filenamebits[filenamebits.length-1];
+        var year = filename.substr(4,4);
+        var month = filename.substr(0,2);
+        var day = filename.substr(2,2);
+        var rest = ".csv";
+        filename = year + "_" + month + "_" + day + rest;
+        casper.download(resource.url, filename);
     }
 });
 
