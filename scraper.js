6fa7c101c7783ac89c7b050579ba399e6bb6fdba
Benjamin Portman <benjamin@orpheusincorporated.com>
2018-01-13 21:35:56 -0500

    added ourDoPostBack to replace provided one

--- scraper.js
+++ scraper.js
@@ -1,39 +1,50 @@
+// set our desired start and end indexes
 var startIndex = 6587;
+var finishIndex = 6500;
+
+// create our main date object
 var d = new Date();
 var dayOfMonth = d.getDate();
-var finishIndex = 6500;
-var ready = true;
-var waiting = false;
 
-function wait(ms){
-    var start = new Date().getTime();
-    var end = start;
-    while(end < start + ms) {
-      end = new Date().getTime();
-   }
- }
-
-// step one day backwards
-while(startIndex > finishIndex) {
-        //dayOfMonth -= 1;
-        d.setDate(--dayOfMonth);
-        startIndex -= 1;
-        
-        // change month
-        if (dayOfMonth == 0) {
-            dayOfMonth = d.getDate();
-            let monthIndex = 'V'+(startIndex-dayOfMonth+1).toString();
-            console.log(monthIndex);
-            __doPostBack('calDateSelection', monthIndex);
-            var monthLoaded = false;
-            while(!monthLoaded) {
-                var monthText = document.getElementsByClassName('TitleStyle')[0].children[0].children[0].children[1].innerHTML;
-                var monthTextId = (new Date(monthText)).getMonth();
-                if(monthTextId == d.getMonth()) {
-                    monthLoaded = true;
-                }
-                console.log(monthText, monthTextId, d.getMonth(), monthLoaded);
-            }
-        }
-        console.log(dayOfMonth, d, startIndex);
+// replicate their function to traverse months and days
+var ourForm = document.forms1;
+function ourDoPostBack(eventTarget, eventArgument) {
+    if (!ourForm) {
+        ourForm = document.form1;
+    }
+    if (!ourForm.onsubmit || (ourForm.onsubmit() != false)) {
+        ourForm.__EVENTTARGET.value = eventTarget;
+        ourForm.__EVENTARGUMENT.value = eventArgument;
+        ourForm.submit();
+    }
 }
+
+// loop until we get to the finish line we set
+while(startIndex > finishIndex) {
+
+    // step backwards 1 day
+    d.setDate(--dayOfMonth);
+    startIndex -= 1;
+    
+    // change month if day is 0
+    if (dayOfMonth == 0) {
+        dayOfMonth = d.getDate();
+        let monthIndex = 'V'+(startIndex-dayOfMonth+1).toString();
+        console.log(monthIndex);
+        ourDoPostBack('calDateSelection', monthIndex);
+        var monthLoaded = false;
+        while(!monthLoaded) {
+            var monthText = document.getElementsByClassName('TitleStyle')[0].children[0].children[0].children[1].innerHTML;
+            var monthTextId = (new Date(monthText)).getMonth();
+            if(monthTextId == d.getMonth()) {
+                monthLoaded = true;
+            }
+            console.log(monthText, monthTextId, d.getMonth(), monthLoaded);
+        }
+    }
+    console.log(dayOfMonth, d, startIndex);
+}
+
+
+
+
