f8ff2e69d720011f1987c0b1446a9e104c3e00b9
Benjamin Portman <benjamin@orpheusincorporated.com>
2018-01-14 00:40:16 -0500

    adjustments that got this working as desired

--- caspertest.js
+++ caspertest.js
@@ -4,30 +4,33 @@ var casper = require('casper').create({
     }
 }); 
 var x = require('casper').selectXPath; 
-
+var downloadcounter = 0;
 casper.userAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36") 
 
-casper.start('https://www.eride.ri.gov/eride2K5/AggregateAttendance/AttendanceReports.aspx'); 
-casper.wait(5000); // probably not necessary 
+casper.start('https://www.eride.ri.gov/eride2K5/AggregateAttendance/AttendanceReports.aspx', function() {
+    for(var j = 0; j < 84; j++) {
+        for(var i = 2;i < 32; i++) {
+            if (i < 10) {
+                var handle = '#GridView1_ctl0'+i+'_imgBtnXls';
+            } else {
+                var handle = '#GridView1_ctl'+i+'_imgBtnXls';
+            }
+
+            if(this.exists(handle)) {
+                this.thenClick(handle);
+                downloadcounter++;
+            }
+        }
+        this.thenClick('a[title="Go to the previous month"]');
+    }
+}); 
 
 casper.on('resource.received', function(resource) {
     casper.echo(resource.url);
-    // if (resource.stage !== "end") {
-    //     return;
-    // }
     if (resource.url.indexOf('DailyAbsenceData.csv') > -1) {
-        casper.echo('this file matches');
         var filenamebits = resource.url.split('/');
         casper.download(resource.url, filenamebits[filenamebits.length-1]);
     }
 });
-
-for(var i = 2;i < 32; i++) {
-    if (i < 10) {
-        casper.thenClick('#GridView1_ctl0'+i+'_imgBtnXls');
-    } else {
-        casper.thenClick('#GridView1_ctl'+i+'_imgBtnXls');
-    }
-}
 
 casper.run(); 
