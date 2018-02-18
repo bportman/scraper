f9cdbf9ee20fa48fca0a38aab787903d4b4f7f1e
Benjamin Portman <benjamin@orpheusincorporated.com>
2018-02-18 18:44:47 -0500

    Various refactoring to fix issue that caused missing files in result

--- caspertest.js
+++ caspertest.js
@@ -8,28 +8,32 @@ var count;
 casper.userAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36") 
 
 casper.start('https://www.eride.ri.gov/eride2K5/AggregateAttendance/AttendanceReports.aspx', function() {
-    for(var j = 0; j < 84; j++) {
+    this.echo("Casper loaded.");
+});
+
+for(var j = 0; j < 84; j++) {
+    casper.then(function() {
         count = this.evaluate(function() { 
             var elements = __utils__.findAll('#GridView1 tr');
             return elements.length; 
         });
         this.echo(count);
-        if(j > 41) { // we had to comment this out first, run til it failed, then uncomment and run again to get complete data
-            for(var i = 2; i <= count; i++) {
-                if (i < 10) {
-                    var handle = '#GridView1_ctl0'+i+'_imgBtnXls';
-                } else {
-                    var handle = '#GridView1_ctl'+i+'_imgBtnXls';
-                }
-                this.echo(handle);
-                if(this.exists(handle)) {
-                    this.thenClick(handle);
-                }
+
+        for(var i = 2; i <= count; i++) {
+            if (i < 10) {
+                var handle = '#GridView1_ctl0'+i+'_imgBtnXls';
+            } else {
+                var handle = '#GridView1_ctl'+i+'_imgBtnXls';
+            }
+            this.echo(handle);
+            if(this.exists(handle)) {
+                this.thenClick(handle);
             }
         }
-        this.thenClick('a[title="Go to the previous month"]');
-    }
-}); 
+    });
+
+    casper.thenClick('a[title="Go to the previous month"]');
+}
 
 casper.on('resource.received', function(resource) {
     casper.echo(resource.url);
